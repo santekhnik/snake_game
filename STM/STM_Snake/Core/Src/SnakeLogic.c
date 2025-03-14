00790f40269a5c4325295147fe4a28f84717d4fd
Bereza Stepan <115149765+yastepdrevobs@users.noreply.github.com>
2025-03-14 15:04:41 +0200

    updated frog appear algorythm
    it is good now

--- STM/STM_Snake/Core/Src/SnakeLogic.c
+++ STM/STM_Snake/Core/Src/SnakeLogic.c
@@ -13,7 +13,8 @@ void randomize_apple(uint8_t *frog_x, uint8_t *frog_y) {
 
 
 uint8_t move_snake(uint8_t command, uint8_t *frog_x, uint8_t *frog_y, uint8_t *payload) {
-    static uint8_t snake_length = 4;
+    uint8_t snake_length = 4;
+    static uint8_t legit = 1;
     static uint8_t x_buffer[128] = {10, 11, 12, 13};
     static uint8_t y_buffer[128] = {15, 15, 15, 15};
 
@@ -33,10 +34,24 @@ uint8_t move_snake(uint8_t command, uint8_t *frog_x, uint8_t *frog_y, uint8_t *p
     }
 
     // Перевірка на "з'їдання" яблука
-    if (x_buffer[0] == *frog_x && y_buffer[0] == *frog_y) {
-        snake_length++;
-        if (snake_length > 128) snake_length = 128;
-        randomize_apple(frog_x, frog_y);  // Генеруємо нове яблуко
+    if (x_buffer[0] == *frog_x && y_buffer[0] == *frog_y) {	//перевірка на з'їдене яблуко
+            snake_length++;
+            legit = 0;
+        	for (uint8_t i = 0; i < snake_length; i++) {
+        		i++;
+        		do {
+        			randomize_apple(frog_x, frog_y);
+        		    legit = 1;
+
+        		    for (uint8_t i = 0; i < snake_length; i++) {
+        		        if (x_buffer[i] == *frog_x && y_buffer[i] == *frog_y) {
+        		            legit = 0;
+        		            break;
+        		        }
+        		    }
+        		} while (!legit);
+        	        }
+            if (snake_length > 255) snake_length = 255;
     }
 
     for (int i = 1; i < snake_length; i++) {
